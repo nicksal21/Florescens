d5bd585eb1abd35b4de2c21a5e91143c7ca70964
nicksal21 <nsalazar1@ufl.edu>
2021-04-06 21:42:59 -0400

    Initial commit

---
+++ src/main/java/com/savagezahar/florescens/Florescens.java
@@ -0,0 +1,11 @@
+package com.savagezahar.florescens;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Florescens implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
